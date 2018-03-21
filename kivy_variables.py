506d5fafeefcbe88ad838b10100ebc0002227a76
Tom Burns <tfburns@users.noreply.github.com>
2018-03-21 23:53:23 +0900

    Add files via upload

--- kivy_variables.py
+++ kivy_variables.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar  1 23:29:28 2018
+Last saved on Wed Mar 21 23:50:00 2018
 
 @author: Tom
 """
@@ -12,17 +13,17 @@ touch_data_list = []
 loaded_line = []
 hover_data = []
 
-# GUI variables
+# GUI variables for guidelines
 X_guideline = []
 Y_guideline = []
 
 # default options
-screen_resolution_width = 800 #px
-screen_resolution_height = 600 #px
+screen_resolution_width = 800      # in pixels
+screen_resolution_height = 600     # in pixels
 save_load_prefix = "touch_data"
-hoverdraw_tickrate = 1 #msec
-guidelines_tickrate = 1 #sec
-hover_key = "h"
-clear_key = "c"
-save_key = "s"
-load_key = "l"
+hoverdraw_tickrate = 1             # in milliseconds
+guidelines_tickrate = 1            # in seconds
+hover_key = 'h'
+clear_key = 'c'
+save_key = 's'
+load_key = 'd'
